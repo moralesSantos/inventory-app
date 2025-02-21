faac0b90f1e04eb002c0ba2a4a9b1d4827b11c35
luis <jmsantos879@gmail.com>
2025-02-21 16:10:51 -0500

    enabled cors

--- inventory-manager/src/main.ts
+++ inventory-manager/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: "http://localhost:3001", 
+    origin: "*", 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization"
   })
